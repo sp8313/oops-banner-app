e90cfd335ddb550d78c524b28d48f948c9a6e207
Soumya Pal <sp8313@srmist.edu.in>
2026-02-17 13:13:05 +0530

    Initial commit: add OOPSBannerAppUC1 Java file

---
+++ OOPSBannerAppUC1.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerApp
+ * Prints the literal text "OOPS" to console.
+ *
+ * @author Soumya
+ * @version 1
+ */
+public class OOPSBannerAppUC1 {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
