ee26ef530653de46feb98284cb502cd6d3b3cf5a
Soumya Pal <sp8313@srmist.edu.in>
2026-02-23 13:15:36 +0530

    Initial commit: add OOPSBannerAppUC2 Java file

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,25 @@
+/**
+ * OOPSBannerApp
+ *
+ * @author Soumya
+ * @version 2
+ */
+public class OOPSBannerUC2 {
+    public static void main(String[] args) {
+
+
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  *****  ");
+
+        System.out.println(" *     * " + " *     * " + " *     * " + " *     * ");
+
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+
+        System.out.println(" *     * " + " *     * " + "  ****** " + "  *****  ");
+
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+
+        System.out.println(" *     * " + " *     * " + " *       " + " *     * ");
+
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
+    }
+}
